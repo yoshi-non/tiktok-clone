640c0de785d743e797da2fb7b663bf0aa3bd75b4
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-16 18:27:13 +0900

    add Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Logo from "../utils/tiktik-logo.png"
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href="/">
+        <div className='w-[100px] md:w-[130px]'>
+            <Image className='cursor-pointer' src={Logo} alt="TikTok" layout='responsive' />
+        </div>
+      </Link>
     </div>
   )
 }
