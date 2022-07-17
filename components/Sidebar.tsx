0ec19271d2d6b6b6de9c7e57ba4c1f7f043dfc35
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-17 16:52:50 +0900

    add Discover

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 import GoogleLogin from 'react-google-login'
 import {AiFillHome, AiOutlineMenu} from "react-icons/ai"
 import {ImCancelCircle} from "react-icons/im"
+import Discover from './Discover'
+import Footer from './Footer'
+import SuggestedAccounts from './SuggestedAccounts'
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true)
@@ -56,8 +59,11 @@ const Sidebar = () => {
           )}
 
           {/* Discover */}
+          <Discover/>
           {/* SuggestedAccounts */}
+          <SuggestedAccounts/>
           {/* Footer */}
+          <Footer/>
         </div>
       )}
     </div>
