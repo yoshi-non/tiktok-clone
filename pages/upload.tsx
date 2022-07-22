a1fb63ac615de9bd6ad69c5a94f08b2d55e2a715
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-22 09:09:07 +0900

    change Upload Page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -74,7 +74,7 @@ const Upload = () => {
 
   return (
     <div className='flex w-full h-full absolute left-0 top-[90px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center'>
-        <div className='bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6'>
+        <div className='bg-white rounded-lg xl:h-[85vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6'>
             <div>
                 <div>
                     <p className='text-2xl font-bold'>Upload Video</p>
